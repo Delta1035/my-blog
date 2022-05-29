b493291922dcb628cd237f9ba1262a6814c0e22f
delta <528491526@qq.com>
2022-05-29 14:45:51 +0800

    init project

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'my-blog';
+  // TODO : 创建一个登陆
+  // FIXME : 修复这里的bug
+}
